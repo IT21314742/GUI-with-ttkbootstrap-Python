07ea5e8fd322cbb0d7c0403cb80b8915bcccff14
IT21314742 <it21314742@my.sliit.lk>
2024-09-27 21:26:12 +0530

    Update
    Update

--- first.py
+++ first.py
@@ -16,7 +16,7 @@ counter = 0
 def changer():
     counter += 1
     if counter % 2 == 0:
-        my_label.config(menu=)
+        my_label.config(text=)
 
 # Create a Label
 my_label = tb.Label(root, text="Hello Vihan!", font=("Poppins", 35),
